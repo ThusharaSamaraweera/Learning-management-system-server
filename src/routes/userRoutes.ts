56501e745f638f4cbb1d472abcc4dd351772ceb8
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-05-24 05:21:10 +0530

    init enroll course endpoint

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,5 +5,6 @@ import { authTokenMiddleware } from "../middlewares/authTokenMiddleware";
 const routes: Router = Router();
 
 routes.get("/", authTokenMiddleware,userController.getAllUsers);
+routes.put("/:userId/courses/:courseId", authTokenMiddleware,userController.enrollToCourse);
 
 export default routes;
